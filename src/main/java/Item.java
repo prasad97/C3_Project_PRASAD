dd94c7dceb90674149910ae3df003e3bdb5f9e14
Prasad Sawant <prasad.sawant20397@gmail.com>
2021-03-01 23:58:10 +0530

    Part 3: Solution

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,10 +1,12 @@
 public class Item {
     private String name;
     private int price;
+    private boolean selected;
 
     public Item(String name, int price) {
         this.name = name;
         this.price = price;
+        this.selected = false;
     }
 
     public String getName() {
@@ -16,5 +18,9 @@ public class Item {
                 + price
                 + "\n"
                 ;
+    }
+
+    public void selectItem(){
+        this.selected = true;
     }
 }
